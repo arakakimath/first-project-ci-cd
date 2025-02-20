0f464b09dda7bd937f68a21c6c32d3c03ca356f4
Matheus Arakaki <arakakimath@gmail.com>
2025-02-20 11:38:50 -0400

    fix: disable environment database_url

--- src/infra/env/env.ts
+++ src/infra/env/env.ts
@@ -1,7 +1,11 @@
 import { z } from 'zod'
 
 export const envSchema = z.object({
-  DATABASE_URL: z.string().url().optional().default('something'),
+  // DATABASE_URL: z
+  //   .string()
+  //   .url()
+  //   .optional()
+  //   .default('something://something.com'),
   PORT: z.coerce.number().optional().default(3333),
 })
 
